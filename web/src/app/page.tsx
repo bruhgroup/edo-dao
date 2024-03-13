27ea52dfbe20f23eba7ed15f5cd66a8e8daf8c60
Vincent <95856449+eve-liya@users.noreply.github.com>
2024-03-12 17:34:23 -1000

    feat: bar graph for proposals

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,41 +1,40 @@
 import Image from "next/image";
 import contract from "@/lib/blockchain";
-import {AbiCoder, Contract, decodeBytes32String, ethers} from 'ethers'
+import {AbiCoder, Contract, decodeBytes32String, ethers, Fragment} from 'ethers'
 import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableFooter,
-  TableHead,
-  TableHeader,
-  TableRow,
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableFooter,
+    TableHead,
+    TableHeader,
+    TableRow,
 } from "@/components/ui/table"
+import Link from "next/link";
+
 export default async function Home() {
-  let a = await contract;
-  const eventAbi = ["event PROPOSAL_SUBMITTED(uint indexed index, address author, string description)"]
-  let events =  await a.queryFilter("PROPOSAL_SUBMITTED", 0);
-  let iface = new ethers.Interface(eventAbi);
-  console.log("Start here");
-  console.log(a.proposals());
-  console.log(events[0]);
-  return (
+    let resolvedContract = await contract;
+    const eventAbi = ["event PROPOSAL_SUBMITTED(uint indexed index, address author, string description)"]
+    let events = await resolvedContract.queryFilter("PROPOSAL_SUBMITTED", 0);
+    let iface = new ethers.Interface(eventAbi);
+    return (
         <Table>
-          <TableCaption>A list of recent proposals.</TableCaption>
-          <TableHeader>
-            <TableRow>
-              <TableHead className="">Proposal</TableHead>
-              <TableHead className="">Author</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {events.map((log, index)=> {
-              return <TableRow key={index}>
-                <TableCell key={index}>Proposal: {iface.parseLog(log)?.args[2]}</TableCell>
-                <TableCell key={index}>By: {iface.parseLog(log)?.args[1]}</TableCell>
-              </TableRow>
-            })}
-          </TableBody>
+            <TableCaption>A list of recent proposals.</TableCaption>
+            <TableHeader>
+                <TableRow>
+                    <TableHead className="">Proposal</TableHead>
+                    <TableHead className="">Author</TableHead>
+                </TableRow>
+            </TableHeader>
+            <TableBody>
+                {events.map((log, index) => {
+                    return <TableRow key={index}>
+                        <TableCell key={index}><Link href={`/${index}`}>Proposal: {iface.parseLog(log)?.args[2]} </Link></TableCell>
+                        <TableCell key={index}>By: {iface.parseLog(log)?.args[1]}</TableCell>
+                    </TableRow>
+                })}
+            </TableBody>
         </Table>
-  );
+    );
 }
--- web/src/lib/blockchain.ts
+++ web/src/lib/blockchain.ts
@@ -1,15 +1,44 @@
-import {Contract, ethers} from 'ethers'
+import {Contract, ethers, Fragment} from 'ethers'
 const contract = (async () => {
     let provider = new ethers.JsonRpcProvider("http://127.0.0.1:8545");
     const Proposal = "(string description, uint256 votesFor, uint256 votesAgainst, mapping(address=>uint8) timesVoted)"
+    let proposalABI = Fragment.from({
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "name": "proposals",
+        "outputs": [
+            {
+                "internalType": "string",
+                "name": "description",
+                "type": "string"
+            },
+            {
+                "internalType": "uint256",
+                "name": "votesFor",
+                "type": "uint256"
+            },
+            {
+                "internalType": "uint256",
+                "name": "votesAgainst",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    })
     let abi = [
         "event AWARDED_TOKENS(address indexed student, uint amount)",
         "event PROPOSAL_SUBMITTED(uint indexed index, address author, string description)",
         "event PROPOSAL_VOTED(uint indexed index, uint8 numVotes, bool support)",
-        "function proposals(uint256) view returns (string, uint256, uint256)"
+        proposalABI
     ];
 
-    let contract = new Contract("0x5FbDB2315678afecb367f032d93F642f64180aa3", abi, provider);
+    let contract = new Contract("0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512", abi, provider);
 // Event listeners
     contract.on("AWARDED_TOKENS", (student, amount) => {
         console.log("AWARDED_TOKENS event received:");
