709cf44ab82bc40a304d1e3e90da79a0f84970d2
echung32 <143231894+echung32@users.noreply.github.com>
2024-04-22 23:21:11 -1000

    chore: remove mentions of edo for privacy

--- solidity/contracts/CLASSDAO.sol
+++ solidity/contracts/CLASSDAO.sol
@@ -1,51 +1,54 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "contracts/EDOKEN.sol";
+import "contracts/CLASSKEN.sol";
 
-contract EDAO is EDOKEN {
+contract CLASSDAO is CLASSKEN {
 
-    address private immutable EDO;
+    address private immutable PROFESSOR;
 
     event AWARDED_TOKENS(address student, uint amount);
     event PROPOSAL_SUBMITTED(uint indexed index, address author, string description);
     event PROPOSAL_VOTED(uint indexed index, uint8 numVotes, bool support);
 
     // Costs 10 tokens to submit a proposal.
-    uint8 immutable ProposalCost = 10;
+    uint8 public immutable PROPOSAL_COST = 10;
 
     struct Proposal {
         string description;
         uint256 votesFor;
         uint256 votesAgainst;
-        mapping(address=>uint8) timesVoted;
+        mapping(address=>uint8) timesAddressVoted;
     }
 
     // Holds all proposals.
     Proposal[] public proposals;
 
-    modifier onlyEDO() {
-        require(msg.sender == EDO);
+    /**
+     * To make certain methods professor-only
+     */
+    modifier onlyProfessor() {
+        require(msg.sender == PROFESSOR);
         _;
     }
 
     /**
-     * Edo has no proposal, voting, or vetoing powers, such that the contract can be trusted.
+     * Professor has no proposal, voting, or vetoing powers, such that the contract can be trusted.
      */
-    modifier noEDO() {
-        require(msg.sender != EDO);
+    modifier notProfessor() {
+        require(msg.sender != PROFESSOR);
         _;
     }
 
-    constructor() EDOKEN() {
-        // Edo is the contract creator.
-        EDO = msg.sender;
+    constructor() CLASSKEN() {
+        // Professor is the contract creator.
+        PROFESSOR = msg.sender;
     }
 
     /**
-     * awardTokens allows ONLY Edo to award governance tokens to students.
+     * awardTokens allows ONLY professor to award governance tokens to students.
      */
-    function awardTokens(address student, uint256 amount) public onlyEDO {
+    function awardTokens(address student, uint256 amount) public onlyProfessor {
         // Mints a number of tokens to the student.
         _mint(student, amount);
 
@@ -53,10 +56,10 @@ contract EDAO is EDOKEN {
     }
 
     /**
-     * submitProposal allows a student to submit an EIP (Edo Improvement Proposal).
+     * submitProposal allows a student to submit an PIP (Professor Improvement Proposal).
      */
-    function submitProposal(string memory description) public noEDO {
-        _burn(msg.sender, ProposalCost);
+    function submitProposal(string memory description) public notProfessor {
+        _burn(msg.sender, PROPOSAL_COST);
 
         Proposal storage newProposal = proposals.push();
         newProposal.description = description;
@@ -69,10 +72,10 @@ contract EDAO is EDOKEN {
      * Multiple votes are allowed, but will have quadratic scaling per vote.
      *  i.e. first vote costs 1, second costs 2, ... n^2
      */
-     function voteProposal(uint proposalIndex, uint8 numVotes, bool support) public noEDO {
+     function voteProposal(uint proposalIndex, uint8 numVotes, bool support) public notProfessor {
         require(proposalIndex < proposals.length);
         
-        uint8 previousVotes = proposals[proposalIndex].timesVoted[msg.sender];
+        uint8 previousVotes = proposals[proposalIndex].timesAddressVoted[msg.sender];
         uint voteCost = (previousVotes + numVotes)^2 - previousVotes^2;
 
         _burn(msg.sender, voteCost);
@@ -82,7 +85,7 @@ contract EDAO is EDOKEN {
             proposals[proposalIndex].votesAgainst += numVotes;
         }
 
-        proposals[proposalIndex].timesVoted[msg.sender] += numVotes;
+        proposals[proposalIndex].timesAddressVoted[msg.sender] += numVotes;
 
         emit PROPOSAL_VOTED(proposalIndex, numVotes, support);
     }
